5ef09da56d3b273bb4c6a64e23a7a7c3de40387b
Donnahue George <Donnahuegjr@gmail.com>
2022-09-29 15:08:49 -0400

    removing timer until bug is fixed

--- src/components/About.js
+++ src/components/About.js
@@ -4,20 +4,33 @@ import myImg from '../imgs/IMG_1616.JPG';
 import Flip from 'react-reveal/Flip';
 // grey #AFB9C5
 const ImgEl = styled('img')(() => ({
-  width: 400,
-  height: 500,
+  width: 300,
+  height: 400,
 }));
 const MyName = styled(Typography)(() => ({
   fontSize: '80px',
   color: '#2B4E63',
   fontFamily: 'monospace',
 }));
+const SubHeader = styled(Typography)(() => ({
+  fontSize: '30px',
+  color: '#2B4E63',
+  fontFamily: 'monospace',
+  maxWidth: '80vw',
+}));
 export default function About() {
-  const [show, setShow] = useState(true);
-  setInterval(() => {
-    if (show) setShow(false);
-    else setShow(true);
-  }, 10000);
+  const [show, setShow] = useState(false);
+  const [count, setCount] = useState(0);
+  // const myTimer = setInterval(() => {
+  //   if (show) setShow(false);
+  //   else setShow(true);
+
+  //   if (count > 5) clearInterval(myTimer);
+  //   else {
+  //     console.log(count);
+  //     setCount(count + 1);
+  //   }
+  // }, 2000);
   return (
     <div>
       <Grid
@@ -26,14 +39,26 @@ export default function About() {
         style={{ height: '40vh', backgroundColor: '#AFB9C5' }}
         alignItems={'center'}
       >
-        <Flip right opposite when={show}>
-          <MyName style={{ transform: 'rotate(45deg)' }}>Donnahue</MyName>
+        <Flip right opposite>
+          <MyName style={{ transform: 'rotate(90deg)', fontSize: 70 }}>
+            Donnahue
+          </MyName>
         </Flip>
 
         <ImgEl src={myImg} />
-        <Flip right opposite delay={1000} when={show}>
-          <MyName style={{ transform: 'rotate(315deg)' }}> George</MyName>
+        <Flip right opposite delay={1000}>
+          <MyName style={{ transform: 'rotate(90deg)' }}> George</MyName>
         </Flip>
+        <Grid container justifyContent={'center'}>
+          <SubHeader>
+            I am a Full Stack Developer leveraging a background in music to
+            build a more intuitive and efficient user experience on the web.
+            Known as an innovative problem-solver passionate about developing
+            apps, with a focus on backend, user interface, and authentication.
+            Strengths in meeting deadlines, understanding documentations and
+            teamwork.
+          </SubHeader>
+        </Grid>
       </Grid>
     </div>
   );
